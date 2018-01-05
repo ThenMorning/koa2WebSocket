a8fec3ff23512b05b25c0e980d1a9156dc23dad4
赵皖华 <wanhua.zhao@hand-china.com>
2018-01-05 13:36:56 +0800

    201801050136

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
+
 /* 实例化外部依赖 */
 let Koa = require("koa2");
 let WebSocket = require("koa-websocket");
 
 /* 实例化 WebSocket, 实例化储存所有上线文数组 并分配监听的端口 */
 let app = WebSocket(new Koa());
-let ctxs = [];
+let conversationList = [];
 app.listen(8200);
+
+function send(opt) {
+    handleConversationList();
+    conversationList.forEach((conversation,index,conversationList)=>{
+        conversation.ctx.websocket.send(opt);
+   })
+}
+
+
+let timer = setInterval(()=>{
+    handleConversationList
+},100);
+
+//create Conversation obj 
+function Conversation(ctx) {
+    let Conversation = new Object();
+    Conversation.ctx = ctx;
+    Conversation.name = '';
+    return Conversation;
+}
+
+function handleConversationList() {    
+    for(let i = 0;i<conversationList.length;i++){
+        if(conversationList[i].ctx.websocket.readyState == 2 || conversationList[i].ctx.websocket.readyState == 3){
+            conversationList.splice(i,1);
+            i--;
+        }
+    }
+}
+
 
 /* 实现简单的接发消息 */
 app.ws.use((ctx, next) => {
-    ctxs.push(ctx);
-    console.log(ctx.websocket._ultron.id);
+    conversationList.push(Conversation(ctx));
+    handleConversationList();
     /*将连接信息存入到数据库中，包括用户名，连接id，时间，消息类型,消息内容*/
-
+    
     var message = null;
     /*转发所有消息，客户端根据消息类型进行选择性渲染*/
     ctx.websocket.on("message", (opt) => {
-        for(let i = 0; i < ctxs.length; i++) {
-            message = JSON.parse(opt);
-            ctxs[i].websocket.send(opt);
+        message = JSON.parse(opt);
+        if(message.type == 'goOnline'){
+            conversationList.forEach((conversation,index,conversationList)=>{
+                 if(conversation.ctx.websocket._ultron.id == ctx.websocket._ultron.id){
+                    conversation.name = message.name;
+                 }
+            })
         }
+        send(opt);
     });
 });
